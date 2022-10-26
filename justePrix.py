6adf24db3f87228bdb490617fee7b8d2fb838a8a
Flotroniks <47397500+Flotroniks@users.noreply.github.com>
2022-10-26 11:50:22 +0200

    feat(dev): ✨ add commentary to the code

--- justePrix.py
+++ justePrix.py
@@ -1,8 +1,8 @@
-print("debut")
-find = False
-tries = 0
-while(find or tries < 25):
-    print("en cours")
-    input_number = input("donnez un nombre : ")
-    tries+=1
-print("fin")
+print("debut") #affiche debut dans le terminal
+find = False # déclare et mets find à flase
+tries = 0 # déclare et mets find à flase
+while(find or tries < 25): # tant que tries < 25
+    print("en cours")# print en cours
+    input_number = input("donnez un nombre : ")# recupere le nombre saisie
+    tries+=1 # augmente tries
+print("fin") #print fin
